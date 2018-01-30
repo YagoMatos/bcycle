677f6180befb7b58bac39ff1478edacfb5d687ba
yagomatos <yago.matos@hotmail.com>
2018-01-30 17:03:25 -0200

    including redux-form

---
+++ frontend/src/billingCycles/billingCyclesForm.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+class BillingCyclesForm extends Component {
+    render(){
+        return(
+            <form role='form'>
+                <div className='box-body'>
+
+                </div>
+                <div className='box-footer'>
+                    <button type='submit' className='btn btn-primary'>SubMit</button>
+                </div>
+            </form>
+        )
+    }
+}
+
+export default BillingCyclesForm
--- frontend/src/main/reducers.js
+++ frontend/src/main/reducers.js
@@ -2,10 +2,14 @@ import { combineReducers } from 'redux'
 
 import DashboardReducer from '../dashboard/dashboardReducer'
 import TabReducer from '../common/tab/tabReducer'
+import BillingCycles from '../billingCycles/billingCyclesReducer'
+import { reducer as formReducer } from 'redux-form'
 
 const rootReducer = combineReducers({
     dashboard: DashboardReducer,
-    tab: TabReducer
+    tab: TabReducer,
+    billingCycles: BillingCycles,
+    form: formReducer
 })
 
 export default rootReducer
